5619d590a73a1baeb4047f040896c270cc3919e1
Leonardo Bitencourt <leonardobitencourt2020@gmail.com>
2024-01-08 09:43:17 -0300

    ajustes

---
+++ wwwroot/js/main.js
@@ -0,0 +1,21 @@
+﻿function init(){
+    fixHeader();
+}
+
+function fixHeader(){
+
+    var header = document.querySelector(".header");
+
+    window.addEventListener("scroll", function(){
+        if(window.scrollY > 1){
+            header.classList.add("active");
+        }else{
+            header.classList.remove("active");
+        }
+    })
+}
+
+
+$(function(){
+    init();
+})
